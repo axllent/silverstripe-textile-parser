49bd73c94fb480079d87e5bba9b754ab585aef5e
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:28:28 +1300

    Converted to PSR-2

--- code/TextileParser.php
+++ code/TextileParser.php
@@ -8,39 +8,40 @@
  * Authors: Techno Joy development team (www.technojoy.co.nz)
  */
 
-class TextileParser extends Extension {
+class TextileParser extends Extension
+{
 
-	static $client = null;
+    public static $client = null;
 
-	static $doctype = 'html5'; // html5 || xhtml
+    public static $doctype = 'html5'; // html5 || xhtml
 
-	static $use_restricted = true; // true / false
+    public static $use_restricted = true; // true / false
 
-	public function Textile($link_urls = true) {
+    public function Textile($link_urls = true)
+    {
+        $content = $this->owner->value;
 
-		$content = $this->owner->value;
+        self::$doctype = (self::$doctype == 'xhtml') ? 'xhtml' : 'html5';
 
-		self::$doctype = (self::$doctype == 'xhtml') ? 'xhtml' : 'html5';
+        if (!self::$client) {
+            self::$client = new \Netcarver\Textile\Parser(self::$doctype);
+        }
 
-		if (!self::$client) self::$client = new \Netcarver\Textile\Parser(self::$doctype);
+        $content = self::$use_restricted ? self::$client->textileRestricted($content, 0) : self::$client->textileThis($content);
 
-		$content = self::$use_restricted ? self::$client->textileRestricted($content, 0) : self::$client->textileThis($content);
+        if ($link_urls) {
+            /* Convert urls to links */
+            $content = preg_replace('#([\s|>|\(|^])(www)#i', '$1http://$2', $content);
+            $pattern = '#(\s|>|\(|^)((http|https|ftp):\/\/[^\s<\)]+)#i';
+            $replacement = '$1<a href="$2">$2</a>';
+            $content = preg_replace($pattern, $replacement, $content);
+            /* Convert all E-mail matches to appropriate HTML links */
+            $pattern = '#([0-9a-z]([-_.]?[0-9a-z])*@[0-9a-z]([-.]?[0-9a-z])*\\.';
+            $pattern .= '[a-wyz][a-z](fo|g|l|m|mes|o|op|pa|ro|seum|t|u|v|z)?)#i';
+            $replacement = '<a href="mailto:\\1">\\1</a>';
+            $content = preg_replace($pattern, $replacement, $content);
+        }
 
-		if ($link_urls) {
-			/* Convert urls to links */
-			$content = preg_replace('#([\s|>|\(|^])(www)#i', '$1http://$2', $content);
-			$pattern = '#(\s|>|\(|^)((http|https|ftp):\/\/[^\s<\)]+)#i';
-			$replacement = '$1<a href="$2">$2</a>';
-			$content = preg_replace($pattern, $replacement, $content);
-			/* Convert all E-mail matches to appropriate HTML links */
-			$pattern = '#([0-9a-z]([-_.]?[0-9a-z])*@[0-9a-z]([-.]?[0-9a-z])*\\.';
-			$pattern .= '[a-wyz][a-z](fo|g|l|m|mes|o|op|pa|ro|seum|t|u|v|z)?)#i';
-			$replacement = '<a href="mailto:\\1">\\1</a>';
-			$content = preg_replace($pattern, $replacement, $content);
-		}
-
-		return $content;
-
-	}
-
+        return $content;
+    }
 }
